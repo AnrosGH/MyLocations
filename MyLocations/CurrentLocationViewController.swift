faef7ee4eb10b1102c2b8c12a41425185d3f84b5
Ed Sasena <ed.sasena@anros.net>
2014-10-20 16:57:25 -0400

    Initial design of the tag screen.

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Ed on 10/20/14.
+//  Copyright (c) 2014 Anros Applications, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var latitudeLabel: UILabel!
+  @IBOutlet weak var longitudeLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var tagButton: UIButton!
+  @IBOutlet weak var getButton: UIButton!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  //#####################################################################
+  // MARK: -
+  // MARK: Action Methods
+  
+  @IBAction func getLocation() {
+    // do nothing yet
+  }
+  //#####################################################################
+}
+
